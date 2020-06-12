f13da47c233dbde0ec3aeaf01a26af1954688ebd
IshitaB08 <f20180306@pilani.bits-pilani.ac.in>
2020-06-13 02:06:00 +0530

    rm

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import simplebot as sb
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 
 
 @app.route("/")
@@ -12,7 +12,7 @@ def home():
 
 @app.route("/get", methods=['POST'])
 def get_bot_response():
-    user_input = request.values.get('body', '').lower()
+    user_input = request.values.get('Body', '').lower()
     resp = MessagingResponse()
     msg = resp.message()
     twily_response = sb.escalation(user_input)
